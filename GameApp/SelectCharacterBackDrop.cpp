dc45046d813d4515f43490120b04cedd15b4d642
designsh <design__sh@naver.com>
2022-04-27 01:23:29 +0900

    신종하 : 업데이트(캐릭터선택화면 임시주석처리)

--- GameApp/SelectCharacterBackDrop.cpp
+++ GameApp/SelectCharacterBackDrop.cpp
@@ -16,8 +16,8 @@ void SelectCharacterBackDrop::Start()
 {
 	float4 WindowSize = GameEngineWindow::GetInst().GetSize();
 
-	BackDrop_ = CreateTransformComponent<GameEngineImageRenderer>(GetTransform());
-	BackDrop_->SetImage("CreateCharacter_BackDrop.png", WindowSize);
+	//BackDrop_ = CreateTransformComponent<GameEngineImageRenderer>(GetTransform());
+	//BackDrop_->SetImage("CreateCharacter_BackDrop.png", WindowSize);
 }
 
 void SelectCharacterBackDrop::Update(float _DeltaTime)
