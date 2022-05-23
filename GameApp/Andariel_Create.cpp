2dfe0dd5d4e3cc76cf15985c0b144514834b02bc
designsh <design__sh@naver.com>
2022-05-23 22:32:24 +0900

    신종하 : 카타콤레벨 최종보스 제작중

--- GameApp/Andariel_Create.cpp
+++ GameApp/Andariel_Create.cpp
@@ -24,29 +24,33 @@ void Andariel::InitAndariel()
 
 void Andariel::TextureCutting()
 {
-	// 대기상태(Fallen_Idle.png, 20x8)
-	GameEngineTexture* Fallen_Idle = GameEngineTextureManager::GetInst().Find("Fallen_Idle.png");
-	Fallen_Idle->Cut(20, 8);
 
-	// 이동상태(Fallen_Walk.png, 10x8)
-	GameEngineTexture* Fallen_Walk = GameEngineTextureManager::GetInst().Find("Fallen_Walk.png");
-	Fallen_Walk->Cut(10, 8);
 
-	// 공격상태(Fallen_Attack.png, 10x8)
-	GameEngineTexture* Fallen_Attack = GameEngineTextureManager::GetInst().Find("Fallen_Attack.png");
-	Fallen_Attack->Cut(10, 8);
 
-	// 피격상태(Fallen_GetHit.png, 7x8)
-	GameEngineTexture* Fallen_GetHit = GameEngineTextureManager::GetInst().Find("Fallen_GetHit.png");
-	Fallen_GetHit->Cut(7, 8);
 
-	// 사망상태(Fallen_Death.png, 20x8)
-	GameEngineTexture* Fallen_Death = GameEngineTextureManager::GetInst().Find("Fallen_Death.png");
-	Fallen_Death->Cut(20, 8);
+	//// 대기상태(Fallen_Idle.png, 20x8)
+	//GameEngineTexture* Fallen_Idle = GameEngineTextureManager::GetInst().Find("Fallen_Idle.png");
+	//Fallen_Idle->Cut(20, 8);
 
-	// 시체상태(Fallen_Dead.png, 8x1)
-	GameEngineTexture* Fallen_Dead = GameEngineTextureManager::GetInst().Find("Fallen_Dead.png");
-	Fallen_Dead->Cut(1, 8);
+	//// 이동상태(Fallen_Walk.png, 10x8)
+	//GameEngineTexture* Fallen_Walk = GameEngineTextureManager::GetInst().Find("Fallen_Walk.png");
+	//Fallen_Walk->Cut(10, 8);
+
+	//// 공격상태(Fallen_Attack.png, 10x8)
+	//GameEngineTexture* Fallen_Attack = GameEngineTextureManager::GetInst().Find("Fallen_Attack.png");
+	//Fallen_Attack->Cut(10, 8);
+
+	//// 피격상태(Fallen_GetHit.png, 7x8)
+	//GameEngineTexture* Fallen_GetHit = GameEngineTextureManager::GetInst().Find("Fallen_GetHit.png");
+	//Fallen_GetHit->Cut(7, 8);
+
+	//// 사망상태(Fallen_Death.png, 20x8)
+	//GameEngineTexture* Fallen_Death = GameEngineTextureManager::GetInst().Find("Fallen_Death.png");
+	//Fallen_Death->Cut(20, 8);
+
+	//// 시체상태(Fallen_Dead.png, 8x1)
+	//GameEngineTexture* Fallen_Dead = GameEngineTextureManager::GetInst().Find("Fallen_Dead.png");
+	//Fallen_Dead->Cut(1, 8);
 }
 
 void Andariel::CreateAnimation()
