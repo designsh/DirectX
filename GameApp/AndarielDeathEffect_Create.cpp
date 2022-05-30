5525f450123c00616314a095b3b61efd651b76b8
designsh <design__sh@naver.com>
2022-05-31 00:42:29 +0900

    신종하 : 업데이트

--- GameApp/AndarielDeathEffect_Create.cpp
+++ GameApp/AndarielDeathEffect_Create.cpp
@@ -18,6 +18,12 @@ void AndarielDeathEffect::TextureCutting()
 	DeathFire2->Cut(20, 1);
 	GameEngineTexture* DeathFire3 = GameEngineTextureManager::GetInst().Find("DeathFire3.png");
 	DeathFire3->Cut(20, 1);
+	//GameEngineTexture* DeathFire1 = GameEngineTextureManager::GetInst().Find("DeathFire1.png");
+	//DeathFire1->Cut(37, 1);
+	//GameEngineTexture* DeathFire2 = GameEngineTextureManager::GetInst().Find("DeathFire2.png");
+	//DeathFire2->Cut(37, 1);
+	//GameEngineTexture* DeathFire3 = GameEngineTextureManager::GetInst().Find("DeathFire3.png");
+	//DeathFire3->Cut(37, 1);
 
 	// 이동방향이 존재하는 불꽃(20x1)
 	GameEngineTexture* DeathFireDir = GameEngineTextureManager::GetInst().Find("DeathFireDir.png");
@@ -33,6 +39,9 @@ void AndarielDeathEffect::CreateAnimation()
 	DeathFire_->CreateAnimation("DeathFire1.png", "Fire1", 0, 19, 0.2f, false);
 	DeathFire_->CreateAnimation("DeathFire2.png", "Fire2", 0, 19, 0.2f, false);
 	DeathFire_->CreateAnimation("DeathFire3.png", "Fire3", 0, 19, 0.2f, false);
+	//DeathFire_->CreateAnimation("DeathFire1.png", "Fire1", 0, 36, 0.1f, false);
+	//DeathFire_->CreateAnimation("DeathFire2.png", "Fire2", 0, 36, 0.1f, false);
+	//DeathFire_->CreateAnimation("DeathFire3.png", "Fire3", 0, 36, 0.1f, false);
 	DeathFire_->SetChangeAnimation("Fire1");
 
 	// 종료시 호출함수 등록
